34d9ad5940024757294d45f95b28aec7b61f7a72
rahul-M-sdet <87rhlmishra@gmail.com>
2023-07-05 16:18:24 +0530

    dropdown test added

--- pageObject/AddToCart.py
+++ pageObject/AddToCart.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 
 class AddToCart:
@@ -11,6 +12,8 @@ class AddToCart:
     button_Remove_xpath = "//button[@id='remove-sauce-labs-fleece-jacket']"
     button_continue_id = "continue-shopping"
     link_button_xpath = "//a[@class='shopping_cart_link']"
+    filter_dropdown_xpath="//select[@class='product_sort_container']"
+
 
     def product_click(self):
         self.driver.find_element(By.XPATH, self.button_addToCart_xpath).click()
@@ -23,3 +26,8 @@ class AddToCart:
 
     def Bucket_click(self):
         self.driver.find_element(By.XPATH, self.link_button_xpath).click()
+
+
+    def click_filter(self, text):
+        drp = Select(self.driver.find_element(By.XPATH, self.filter_dropdown_xpath))
+        drp.select_by_visible_text(text)
